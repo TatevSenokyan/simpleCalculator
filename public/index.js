e09e4706e1e4a4c218197871b70f762472c4bb47
tatevik <tatevik.senokyan@gmail.com>
2021-10-05 19:05:41 +0400

    add backspace keyboard event

--- public/index.js
+++ public/index.js
@@ -254,6 +254,7 @@ function calcPercent() {
   // adding keyboard events
 
    document.onkeyup=e=>{
+    
      if(e.key=='0' || e.key=='1' || e.key=='2' || e.key=='3' || e.key=='4' || e.key=='5' || e.key=='6'
      || e.key=='7' || e.key=='8' || e.key=='9' || e.key=='.' || e.key=='-' || e.key=='+' || e.key=='/' || e.key=='*'
      ) {
@@ -262,7 +263,7 @@ function calcPercent() {
        result()
      } else if (e.key=='*' || e.key=='Num*') {
       addEventValue(e.key)
-     } else if (e.key=='c' || e.key=='C') {
+     } else if (e.key=='Backspace') {
        clearResult ()
      } else if (e.key=='%') {
        calcPercent()
